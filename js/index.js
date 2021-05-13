ad755eae8d960c3879816b9e08c7a8556bd2801d
hckillian <hannahck98@gmail.com>
2021-05-13 08:15:56 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/*eslint-env es6*/
+/*eslint-env browser*/
+
+/*
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+*/
